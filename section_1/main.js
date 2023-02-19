a5340075aad56f54e26ac839afc726f8f73000f1
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-02-19 16:09:45 +0700

    working with functions

---
+++ section_1/main.js
@@ -0,0 +1,14 @@
+/**
+Hàm trong JS 
+1. Hàm?
+- Là một khối mã
+- làm 1 việc cụ thể
+2. Loại hàm
+- Built-in
+- Tự định nghĩa
+3. Tính chất
+- Không thực thi khi định nghĩa
+- Sẽ thực thi khi được gọi
+- Có thể nhận tham số
+- Có thể trả về một giá trị
+ */
