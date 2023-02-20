60eb03377fa7347dc8c9fae01023784b5734515d
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-02-20 19:55:03 +0700

    functions types

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ```
 1. Section 1: Ôn lại khái niệm hàm
 2. Section 2: Tham số trong hàm
+3. Section 3: Hiểu hơn về hàm
+4. Section 4: Các loại hàm
---
+++ chapter_3/section_4/main.js
@@ -0,0 +1,21 @@
+/*
+Các loại function
+
+1. Declare function
+2. Expression function
+3. Arrow function
+*/
+
+//Ví dụ declare function
+function showMessage() {
+
+}
+
+//Ví dụ Expression function 
+var showMessage2 = function () {
+
+}
+
+setTimeout(function autoLogin() {
+
+});
